3a396e0a726762ee3eadaea250af8b0d38ff8874
MohammadJS2642 <mohammadjs2642@gmail.com>
2021-07-14 13:42:52 +0430

    create a ShowData class and inside that def for get method api

--- backend/backendproject/apiApp/views.py
+++ backend/backendproject/apiApp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import serializers
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .serializers import DidYouKnowSerializers
+from ModelsDB.models import DidYouKnow
 
 # Create your views here.
+
+
+class ShowData(APIView):
+    def get(self, request, *args, **kwargs):
+        querySets = DidYouKnow.objects.al()
+        serializers = DidYouKnowSerializers(querySets, many=True)
+
+        return Response(serializers.data)
