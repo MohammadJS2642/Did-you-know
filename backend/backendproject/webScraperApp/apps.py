034bd0489e149be22207cdbd4a2cf18ea2a70c6e
MohammadJS2642 <mohammadjs2642@gmail.com>
2021-07-14 20:13:23 +0430

    schedulerApp added to apps.py

--- backend/backendproject/webScraperApp/apps.py
+++ backend/backendproject/webScraperApp/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class WebscraperappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'webScraperApp'
+
+    def ready(self):
+        from schedulerApp import updater
+        updater.starter()
