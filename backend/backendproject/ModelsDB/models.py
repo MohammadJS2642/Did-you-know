ed9a7e9571c1cc628d1c0792116dad16e0d6383f
MohammadJS2642 <mohammadjs2642@gmail.com>
2021-07-15 19:41:28 +0430

    created_at and updated_at column added to database with models

--- backend/backendproject/ModelsDB/models.py
+++ backend/backendproject/ModelsDB/models.py
@@ -5,4 +5,5 @@ from django.db import models
 
 class DidYouKnow(models.Model):
     text = models.TextField()
-    created_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
