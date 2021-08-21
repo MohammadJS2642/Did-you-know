021e526074f304608075a72e48c7c6bd602535e8
MohammadJS2642 <mohammadjs2642@gmail.com>
2021-08-21 10:10:03 +0430

    for creating again project deleted

--- backend/backendproject/schedulerApp/updater.py
+++
@@ -1,9 +0,0 @@
-from apscheduler.schedulers .background import BackgroundScheduler
-from datetime import datetime
-from . import jobs
-
-
-def starter():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(jobs.getter, 'interval', seconds=1440)
-    scheduler.start()
